ec152a6099abe2979f994f6f20a8741422dee7e3
vsaravanan1 <vsaravanan@ufl.edu>
2025-11-07 16:47:38 -0500

    fixed stuff

--- ABDQ.hpp
+++ ABDQ.hpp
@@ -44,23 +44,122 @@ public:
         other.back_ = 0;
     }
     ABDQ& operator=(const ABDQ& other) {
+        if (this == &other) return *this;
+        delete [] data_;
+        data_ = new T[other.capacity_];
+        for(int i = 0; i < other.capacity_; i++) {
+            data_[i] = other.data_[i];
+        }
+
+        size_ = other.size_;
+        capacity_ = other.capacity_;
+        front_ = other.front_;
+        back_ = other.back_;
     }
-    ABDQ& operator=(ABDQ&& other) noexcept;
-    ~ABDQ() override;
+    ABDQ& operator=(ABDQ&& other) noexcept {
+        if (this == &other) return *this;
+        delete [] data_;
+        data_ = other.data_;
+        size_ = other.size_;
+        capacity_ = other.capacity_;
+        front_ = other.front_;
+        back_ = other.back_;
+        other.data_ = nullptr;
+        other.size_ = 0;
+        other.front_ = 0;
+        other.back_ = 0;
+        other.capacity_ = 0;
+    }
+
+    ~ABDQ() override {
+        delete [] data_;
+        data_ = nullptr;
+    }
 
     // Insertion
-    void pushFront(const T& item) override;
-    void pushBack(const T& item) override;
+    void pushFront(const T& item) override {
+        ensureCapacity();
+        front_ = (front_ - 1)%capacity_;
+        data_[front_] = item;
+        size_++;
+    }
+    void pushBack(const T& item) override {
+        ensureCapacity();
+        back_ = (back_+1)%capacity_;
+        data_[(back_-1)%capacity_] = item;
+        size_++;
+    }
 
     // Deletion
-    T popFront() override;
-    T popBack() override;
+    T popFront() override {
+        if (getSize() == 0) {
+            throw std::runtime_error("empty deque");
+        }
+        T frontData = data_[front];
+        front_ = (front_+1)%capacity_;
+        size_--;
+        shrinkIfNeeded();
+        return frontData;
+    }
+    T popBack() override {
+        if (getSize() == 0) {
+            throw std::runtime_error("empty deque");
+        }
+        T backData = data_[(back_-1)%capacity_];
+        back_ = (back_ - 1)%capacity_;
+        size_--;
+        shrinkIfNeeded();
+        return backData;
+    }
 
     // Access
-    const T& front() const override;
-    const T& back() const override;
+    const T& front() const override {
+        if (getSize() == 0) {
+            throw std::runtime_error("empty deque");
+        }
+        return data_[front_%capacity_];
+    }
+    const T& back() const override {
+        if (getSize() == 0) {
+            throw std::runtime_error("empty deque");
+        }
+        return data_[back_%capacity_ - 1];
+    }
 
     // Getters
-    std::size_t getSize() const noexcept override;
+    std::size_t getSize() const noexcept override {
+        return size_;
+    }
 
+    std::size_t getMaxCapacity() const noexcept override {
+        return capacity_;
+    }
+
+    void resize(std::size_t newCap) {
+        T* newData = new T[newCap];
+        for (int i = 0; i < size_; i++) {
+            newData[i] = data_[(front_+i)%capacity_];
+        }
+
+        front_ = 0;
+        back_ = size_%newCap;
+        delete [] data_;
+        data_ = newData;
+    }
+
+    void ensureCapacity() {
+        if (getSize() >= getMaxCapacity()) {
+            resize(capacity_*2);
+        }
+    }
+
+    void shrinkIfNeeded() {
+        if (getMaxCapacity() == 1) {
+            return;
+        }
+
+        if ((getMaxCapacity() == 2 && getSize() <= getMaxCapacity()/2) || getSize() <= getMaxCapacity()/4) {
+            resize(getMaxCapacity()/2);
+        }
+    }
 };
